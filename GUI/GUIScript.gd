75332ed97c02d232a34fb4d60bb3d83977e998f5
Pacihstan <pax@isphere.net>
2023-02-20 14:38:03 -0600

    Commit notes: Full rework of gates and movement - full rework of gate structure and functionality     - forced to have type when instanced     - helper function to set gate type at any time (allows for switching type after instancing)     - now distincly detects a single click vs a click and drag (this matters for selection functionality) - Full rework of movement system for instanced gates     - instead of using a software cursor, better solution, use the instanced gate as the cursor         - switch to FPS-like control scheme where mouse is captured in center of screen and hidden when moving a gate           and input from captured mouse is used to move gate         - mouse is shown when gate is dropped at its position relative to the gate when picked up (offset based) - Full rework of hover system     - Reference to instanced node is passed to main Node2D from node when said node is hovered over         - This gives the specific node being hovered over and allows changes to be made to its     - Blue highlight when hovered over (will make this look better in the future)

--- GUI/GUIScript.gd
+++ GUI/GUIScript.gd
@@ -9,12 +9,7 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.connect("mouse_entered", get_node("/root/Node2D"), "OverSelectableScene", [self])
-	self.connect("mouse_exited", get_node("/root/Node2D"), "LeftSelectableScene", [null])
-
-
-
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Gate/Gate.gd
+++ Gate/Gate.gd
@@ -1,40 +1,27 @@
 extends KinematicBody2D
 
-var selected = false
 var offset
 var frame = 0
-var type = null
+var type = "OR"
 var gateSprite = Sprite.new()
 
 
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-		if Input.is_action_just_pressed("click]"):
-			selected = true
-			offset = get_global_mouse_position() - global_position
-			
-			
-			
-			
-func _physics_process(delta): #consider using *_input (not the same as _input)
-	if selected:
+	if Input.is_action_just_pressed("click]"):
+		offset = get_global_mouse_position() - global_position
+
+func _physics_process(delta): # should be used when querying the state of an input (in this case, to query whether left click is still pressed)
+	if Input.is_mouse_button_pressed(1):
 		#global_position = lerp(global_position, get_global_mouse_position() - offset, 25 * delta)
 		var direction = (get_global_mouse_position() - position) - offset
 		move_and_collide(direction)
 		
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and not event.pressed:
-			selected = false
-	
 func set_Gate_Type(type):
 	gateSprite.texture = load("res://Gate/" + type + ".png")
 	self.add_child(gateSprite)
-
 
 func _ready():
 	set_Gate_Type(type)
 	$Area2D.connect("mouse_entered", get_parent(), "OverSelectableScene", [self])
 	$Area2D.connect("mouse_exited", get_parent(), "LeftSelectableScene", [null])
-	
