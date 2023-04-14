1bf5071364e5483f7a7b5b752d6a4ed3e78ea84a
Pacihstan <pax@isphere.net>
2023-04-14 00:53:37 -0500

    Non-Intuitively added inputs and outputs and the ability for them to connect to pins. Need to change code to reflect how different gates are from inputs and outputs

--- GUI/GUIScript.gd
+++ GUI/GUIScript.gd
@@ -1,11 +1,10 @@
-extends MarginContainer
+extends Node2D
 var typeList = ['AND', 'Buffer', 'NAND', 'NOR', 'NOT', 'OR', 'XNOR', 'XOR']
+var otherTypeList = ['IN', 'OUT']
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,26 +15,32 @@ func _ready():
 		currentGateSelectionLabel.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER);
 		currentGateSelectionLabel.add_theme_color_override("font_color", Color(0.0, 0.0, 0.0, 1.0))
 		# set color to black or something here
-		$VBoxContainer.add_child(currentGateSelectionLabel)
+		get_node("HBoxContainer/Control/VBoxContainer").add_child(currentGateSelectionLabel)
 		
 		var currentGateTextureButton = TextureButton.new()
 		currentGateTextureButton.texture_normal = load("res://Gate/" + gateType + ".png")
 		currentGateTextureButton.stretch_mode = TextureButton.STRETCH_KEEP_CENTERED
-		$VBoxContainer.add_child(currentGateTextureButton)
+		get_node("HBoxContainer/Control/VBoxContainer").add_child(currentGateTextureButton)
 		
 		#now manually add trigger
 		currentGateTextureButton.connect("pressed",Callable(get_node("/root/Node2D"),"SetSelectedGate").bind(gateType))
+		
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
-
-
-
-
-#func _on_ANDButton_pressed():
-	#get_node("/root/Node2D").SelectedGate("AND")
+	for otherType in otherTypeList:
+		var currentOtherSelectionLabel = Label.new()
+		currentOtherSelectionLabel.text = otherType
+		currentOtherSelectionLabel.set_vertical_alignment(VERTICAL_ALIGNMENT_CENTER);
+		currentOtherSelectionLabel.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER);
+		currentOtherSelectionLabel.add_theme_color_override("font_color", Color(0.0, 0.0, 0.0, 1.0))
+		get_node("HBoxContainer//Control2/VBoxContainer").add_child(currentOtherSelectionLabel)
+		
+		var currentOtherTextureButton = TextureButton.new()
+		currentOtherTextureButton.texture_normal = load("res://Input_Output/BodyX.png")
+		currentOtherTextureButton.stretch_mode = TextureButton.STRETCH_KEEP_CENTERED
+		get_node("HBoxContainer//Control2/VBoxContainer").add_child(currentOtherTextureButton)
+		
+		#now manually add trigger
+		currentOtherTextureButton.connect("pressed",Callable(get_node("/root/Node2D"),"SetSelectedGate").bind(otherType))
+		
+	#set location of second vbox accordingly
+	#print(get_node("Control").position)
--- MainScene.gd
+++ MainScene.gd
@@ -14,15 +14,11 @@ var isMousePositioned = true #had to add this because of timing issues. Might be
 var gateReferencesByCategory = {}
 var connectionsList = []
 var inputsAndOutputsList = []
-
 var mouseOverAndSelectionCast = ShapeCast2D.new()
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Create dictionary containing arrays that will hold references to all gates for every gate type
-	#Figure out a better way to set up all of the lists
 	gateReferencesByCategory['AND'] = []
 	gateReferencesByCategory['Buffer'] = []
 	gateReferencesByCategory['NAND'] = []
@@ -40,18 +36,12 @@ func _ready():
 	mouseOverAndSelectionCast.collide_with_areas = true
 	self.add_child(mouseOverAndSelectionCast)
 
-
-
-
-
-
-
-
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta): #combined old physics_process with process, need to reorganize contents
 	#Get node currently over every frame using raycast
 	mouseOverAndSelectionCast.position = get_global_mouse_position()
+	
+	
 	
 	#detect is mouse if over last node after drag and drop
 	if mouseOverAndSelectionCast.get_collision_count() > 0 and mouseOverAndSelectionCast.get_collider(0) == scene_currently_over:
@@ -60,15 +50,17 @@ func _process(delta): #combined old physics_process with process, need to reorga
 	if !Input.is_key_pressed(KEY_CTRL):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and Input.is_mouse_button_pressed(1): 
 			isMousePositioned = false
+			#add if here
 			UpdateConnectionCoordinates(scene_currently_over)
 			scene_currently_over.move_and_collide(scene_currently_over.direction)
 			scene_currently_over.direction = Vector2(0,0)
+			if "Input_Output" in scene_currently_over.name:
+				scene_currently_over.get_node("Input_Output_Label").position = scene_currently_over.position
 			
 		elif Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 			Input.warp_mouse(scene_currently_over.position + scene_currently_over.offset)
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
 	
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		scene_currently_over.direction += mouseDelta* (Performance.get_monitor(Performance.TIME_FPS)) * delta # current position of mouse relative to last * current fps * time since last frame. This allows for consistent movement reguardless of framerate
@@ -84,8 +76,6 @@ func _process(delta): #combined old physics_process with process, need to reorga
 		if !Input.is_key_pressed(KEY_CTRL): #adding another not ctrl section to make it more obvious
 			newConnection = null
 	
-	
-
 func UpdateConnectionCoordinates(sceneToUpdate): # updates coordinates of and redraws connection as participating gate is moved
 	var connectionPointIndex = 0
 	for pinType in sceneToUpdate.inAndOutPINListDictionary:
@@ -99,15 +89,6 @@ func UpdateConnectionCoordinates(sceneToUpdate): # updates coordinates of and re
 						gatePIN.connectionParticipatingIn.queue_redraw ()
 					connectionPointIndex += 1
 				connectionPointIndex = 0
-	
-	
-	
-
-func OverSelectionGUI(): #pause everything except for selectionGUI when inside of it
-	get_tree().paused = true
-func LeftSelectionGUI():
-	get_tree().paused = false
-	
 
 func OverSelectableScene(currently_over): #sets scene_currently_over to a reference of the instanced scene currently being hovered over
 	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
@@ -116,80 +97,64 @@ func OverSelectableScene(currently_over): #sets scene_currently_over to a refere
 		scene_currently_over.set_physics_process(true)
 		if currently_over == scene_currently_over:
 			scene_currently_over.Blank.visible = true
-		
-		
+			
 func LeftSelectableScene():
 	scene_currently_over.Blank.visible = false
 	scene_currently_over.set_physics_process(false)
 	scene_currently_over = null
-		
-		
-		
-# add, remove, and clear selected gates
-# selection list should be cleared when blank canvas is clicked or gate is clicked
-# when gate is clicked, add to selection list
-func SelectGates():#Add list of gates (most of the time its just going to be one) to list of currently selected gates
-	pass
-	
-func DeselectGate(): #Remove gate from list of selected gates (bc selection can only remove one at a time. use windows desktop for reference)
-	pass
 		
 func SelectedGate(scenePath):
 	selectedGate = scenePath
 
 func _input(event):
 	if event.is_action_pressed("click]") and selectedGate != null and mouseOverAndSelectionCast.get_collision_count() == 0 and !Input.is_key_pressed(KEY_CTRL):
-		var gate_instance = load("res://Gate/Gate.tscn").instantiate()
-		gate_instance.type = selectedGate
-		gate_instance.position = get_local_mouse_position()
-		$GateContainer.add_child(gate_instance)
-		gate_instance.move_and_collide(Vector2(0,0))
+		if selectedGate != "IN" and selectedGate != "OUT":
+			var gate_instance = load("res://Gate/Gate.tscn").instantiate()
+			gate_instance.type = selectedGate
+			gate_instance.position = get_local_mouse_position()
+			$GateContainer.add_child(gate_instance)
+			gate_instance.move_and_collide(Vector2(0,0))
 		
-		#naming of Gates upon creation
-		#need to add a way to change the name later, but changing requires enforcement of naming conventions (why im skipping it for now)
-		gate_instance.gateName = gate_instance.type.to_lower() + str(CountExistingGatesOfType(gate_instance.type)+1)
-		gateReferencesByCategory[gate_instance.type].append(gate_instance)
+			#naming of Gates upon creation
+			#need to add a way to change the name later, but changing requires enforcement of naming conventions (why im skipping it for now)
+			gate_instance.gateName = gate_instance.type.to_lower() + str(CountExistingGatesOfType(gate_instance.type)+1)
+			gateReferencesByCategory[gate_instance.type].append(gate_instance)
+		else:
+			var in_out_instance = load("res://Input_Output/Input_Output.tscn").instantiate()
+			in_out_instance.type = selectedGate
+			in_out_instance.position = get_local_mouse_position()
+			$GateContainer.add_child(in_out_instance)
+			in_out_instance.move_and_collide(Vector2(0,0))
+			
 	
-		
-		
-	#just some tests here to see how i can make this functional
+
 	elif event is InputEventMouseMotion and scene_currently_over is CharacterBody2D:
 		mouseDelta = event.relative#Input.get_last_mouse_velocity()
 			
-
-			
-func CreatePINConnection(PIN):
+func CreatePINConnection(PIN): #change function to also take in the gate the pin is in as well and use that instead of scene_currently_over
 	if Input.is_key_pressed(KEY_CTRL):
+		
 		if newConnection == null:
 			newConnection = load("res://Connection/Connection.tscn").instantiate()
 			newConnection.get_node("Line2D").set_default_color(Color(0,1,1,1))
 			newConnection.get_node("Line2D").width = 5
-			newConnection.get_node("Line2D").add_point(scene_currently_over.position + PIN.position + PIN.get_node("Area2D/CollisionShape2D").position)
+			if "Input_Output" not in PIN.get_parent().name:
+				newConnection.get_node("Line2D").add_point(scene_currently_over.position + PIN.position + PIN.get_node("Area2D/CollisionShape2D").position)
+			else:
+				newConnection.get_node("Line2D").add_point(scene_currently_over.position + scene_currently_over.get_node("BodyX").position)
 			newConnection.get_meta("nodesConnected")[0] = PIN
-		elif newConnection != null:
-			newConnection.get_node("Line2D").add_point(scene_currently_over.position + PIN.position + PIN.get_node("Area2D/CollisionShape2D").position)
+		elif newConnection != null and scene_currently_over != null: #this is a bandaid fix
+			print(PIN.get_parent().name)
+			if "Input_Output" not in PIN.get_parent().name:
+				newConnection.get_node("Line2D").add_point(scene_currently_over.position + PIN.position + PIN.get_node("Area2D/CollisionShape2D").position)
+			else:
+				newConnection.get_node("Line2D").add_point(scene_currently_over.position + scene_currently_over.get_node("BodyX").position)
 			newConnection.get_meta("nodesConnected")[1] = PIN
 			$GateContainer.add_child(newConnection)
 			connectionsList.append(newConnection)
 			newConnection.get_meta("nodesConnected")[0].connectionParticipatingIn = newConnection
 			newConnection.get_meta("nodesConnected")[1].connectionParticipatingIn = newConnection
 			newConnection = null
-
-func SetSelectedGate(gateTypeIn):
-	selectedGate = gateTypeIn
-
-
-func _on_gd_example_position_changed(node, new_pos):
-	#print("The position of " + node.get_class() + " is now " + str(new_pos))
-	pass
-
-
-func _on_gd_example_cpp_print_signal(node, print_string):
-	#print("CPP Print: " + print_string)
-	pass
-
-
-
 
 
 func _on_simulate_logic_button_pressed():
@@ -214,8 +179,8 @@ func _on_simulate_logic_button_pressed():
 		myFile.store_line("")
 
 	myFile.store_line("ALIASES");
-	myFile.store_line("CONNECTIONS");
 	
+	myFile.store_line("CONNECTIONS");
 	myFile.store_line("")
 	for connection in connectionsList:
 		myFile.store_string(str(connection.get_meta("nodesConnected")[0].get_parent().gateName, "#", connection.get_meta("nodesConnected")[0].get_meta("PINNo")))
@@ -228,10 +193,25 @@ func _on_simulate_logic_button_pressed():
 	myFile = null
 						
 						
-
-
 func CountExistingGatesOfType(gateType):
 	var gateCount = 0
 	for gateNode in gateReferencesByCategory[gateType]:
 		gateCount+=1
 	return gateCount
+
+func OverSelectionGUI(): #pause everything except for selectionGUI when inside of it
+	get_tree().paused = true
+func LeftSelectionGUI():
+	get_tree().paused = false
+
+
+func SetSelectedGate(gateTypeIn):
+	selectedGate = gateTypeIn
+
+func _on_gd_example_position_changed(node, new_pos):
+	#print("The position of " + node.get_class() + " is now " + str(new_pos))
+	pass
+
+func _on_gd_example_cpp_print_signal(node, print_string):
+	#print("CPP Print: " + print_string)
+	pass
