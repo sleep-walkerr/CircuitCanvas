9b314ea2d37014c3a8c0bcae5c4aed6f8db3f5b1
Pacihstan <pax@isphere.net>
2023-05-01 14:57:56 -0500

    Changed icon sizes for selection menu and GUI script no instances in selection entries evenly

--- GUI/GUIScript.gd
+++ GUI/GUIScript.gd
@@ -1,12 +1,17 @@
 extends Panel
-var typeList = ['AND', 'Buffer', 'NAND', 'NOR', 'NOT', 'OR', 'XNOR', 'XOR']
-var otherTypeList = ['IN', 'OUT', 'Delete']
+var typeList = ['AND', 'NAND', 'NOR', 'NOT', 'OR', 'XNOR', 'XOR', 'IN', 'OUT', 'Delete']
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# currentOtherTextureButton.connect("pressed",Callable(get_node("/root/Node2D"),"SetSelectedGate").bind(currentOtherTextureButton))
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	for type in typeList:
+		var newGateSelectionContainer = load("res://GUI/GateSelectionContainer.tscn").instantiate()
+		newGateSelectionContainer.get_node("LabelPanel/GateSelectionLabel").text = type
+		newGateSelectionContainer.get_node("GateButtonPanel/GateSelectionButton").texture_normal = load("res://Icons/" + type + ".png")
+		newGateSelectionContainer.get_node("GateButtonPanel/GateSelectionButton").texture_pressed = load("res://Icons/" + type + "Pressed.png")
+		newGateSelectionContainer.get_node("GateButtonPanel/GateSelectionButton").connect("pressed",Callable(get_node("/root/Node2D"),"SetSelectedGate").bind(newGateSelectionContainer))
+		newGateSelectionContainer.name = type + "SelectionContainer"
+		newGateSelectionContainer.set_meta("type", type)
+		$SelectionButtonsContainer.add_child(newGateSelectionContainer)
