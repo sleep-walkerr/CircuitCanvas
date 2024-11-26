d92a5ef5cf9998402f6b09bd578e98f245929616
Paxton <pax@isphere.net>
2024-11-26 17:59:44 -0600

    now counting highest pin match count for later use

--- Export/export_to_sdl.gd
+++ Export/export_to_sdl.gd
@@ -77,7 +77,6 @@ func SimplifyWireConnections() -> void:
 		# if wires havent been replaced yet, replace according to pin_match entry
 		if WireConnections.has(pin_match[0]) and WireConnections.has(pin_match[1]):
 			var simplified_wire = pin_match[2]
-			print("simplified wire:", simplified_wire)
 			# check other matches/simplifications for the pin erased
 			for other_pin_match in MatchesFound:
 				var pin_deleted = pin_match[3]
@@ -97,11 +96,10 @@ func SimplifyWireConnections() -> void:
 						simplified_wire.append(good_pin)
 						WireConnections.erase(other_pin_match[0])
 						WireConnections.erase(other_pin_match[1])
-						print("pin_deleted",pin_deleted," bad_pin:",bad_pin,"good_pin:",good_pin)
 					elif other_pin_match[1].has(pin_deleted):
 						bad_pin = other_pin_match[1]
 						bad_pin.erase(pin_deleted)
-						bad_pin = bad_pin[1]
+						bad_pin = bad_pin[0]
 						good_pin = other_pin_match[0]
 						good_pin.erase(bad_pin)
 						good_pin = good_pin[0]
@@ -110,16 +108,89 @@ func SimplifyWireConnections() -> void:
 						simplified_wire.append(good_pin)
 						WireConnections.erase(other_pin_match[0])
 						WireConnections.erase(other_pin_match[1])
-						print("pin_deleted",pin_deleted," bad_pin:",bad_pin,"good_pin:",good_pin)
-			print("simplified wire after changes:", simplified_wire)
 			WireConnections.erase(pin_match[0])
 			WireConnections.erase(pin_match[1])
 			WireConnections.append(simplified_wire)
-	 
-	#print("matches list:")
-	#for pin_match in MatchesFound:
-		#print(pin_match, PinMatches.count(pin_match[3]))
-	#print("end list")
+	# END STEP 1 IN ALGORITHM
+	# ***********************************************************************************************
+	# ALGORITHM STEP 2
+	# make list with all pins that are making contact with a gate or input/output
+	var ContactPins = []
+	for wire_connection in WireConnections:
+		for pin in wire_connection:
+			# if pin is making contact with a gate pin
+			if GatesContainer.get_cell_source_id(pin) == 8 or GatesContainer.get_cell_source_id(pin) == 7:
+				ContactPins.append(pin)
+			# if pin is making contact with any input or output pin
+			for IOObject in InputsOutputsContainer.get_children():
+				if IOObject.get_cell_source_id(pin) == 3:
+					ContactPins.append(pin)
+	# Generate a new list of matches, matches cannot be added if their wires contain a pin inside of ContactPins
+	MatchesFound.clear()
+	PinMatches.clear()
+		# collect all matches
+	for connection in WireConnections:
+			for other_connection in WireConnections:
+				if other_connection != connection:
+					if other_connection.has(connection[0]):
+						var new_pin1 = other_connection.duplicate()
+						var new_pin2 = connection.duplicate()
+						new_pin1.erase(connection[0])
+						new_pin2.erase(connection[0])
+						var new_connection = [new_pin1[0],new_pin2[0]]
+						#if both new pins are in the ContactPins list, dont allow the match
+						if !ContactPins.has(new_pin1[0]) and !ContactPins.has(new_pin2[0]):
+							MatchesFound.append([connection, other_connection, new_connection, connection[0]])
+					if other_connection.has(connection[1]):
+						var new_pin1 = other_connection.duplicate()
+						var new_pin2 = connection.duplicate()
+						new_pin1.erase(connection[1])
+						new_pin2.erase(connection[1])
+						var new_connection = [new_pin1[0],new_pin2[0]]
+						#if both new pins are in the ContactPins list, dont allow the match
+						if !ContactPins.has(new_pin1[0]) and !ContactPins.has(new_pin2[0]):
+							MatchesFound.append([connection, other_connection, new_connection, connection[1]])
+	# remove match duplicates
+	for pin_match in MatchesFound:
+		for other_pin_match in MatchesFound:
+			# dont compare them to themselves
+			if pin_match != other_pin_match:
+				if other_pin_match[2].has(pin_match[2][0]) and other_pin_match[2].has(pin_match[2][1]):
+					MatchesFound.erase(other_pin_match)
+	# count matches for a given pin and put them in a struct
+	for pin_match in MatchesFound:
+		PinMatches.append(pin_match[3])
+	# count the PinMatches and order them by occurrence
+	var pin_matches_by_occurance = []
+	var pin_occurrance_mask = []
+	var pin_occurance_final = []
+	for pin_match in MatchesFound: # get unique pin_matches
+		if !pin_matches_by_occurance.has(pin_match[3]):
+			pin_matches_by_occurance.append(pin_match[3])
+	# collect # of times each pin occurrs in a match
+	for pin_match in pin_matches_by_occurance:
+		pin_occurrance_mask.append(PinMatches.count(pin_match))
+	pin_occurrance_mask.sort()
+	# replace each # with its respective pin match
+	for pin_occurance_number in pin_occurrance_mask:
+		for pin_match in pin_matches_by_occurance:
+			if PinMatches.count(pin_match) == pin_occurance_number:
+				pin_occurance_final.append([pin_match, pin_occurance_number])
+				break
+	pin_occurance_final.reverse()
+	print("pin occurance final:", pin_occurance_final)
+	for matched_pin in pin_occurance_final:
+		for proposed_match in MatchesFound:
+			#if proposed_match[0].has(matched_pin[0]) or 
+			pass
+	
+	
+	
+	
+	print("matches list:")
+	for pin_match in MatchesFound:
+		print(pin_match," ", PinMatches.count(pin_match[3]))
+	print("end list")
 	
 	print("wire connections final:")
 	for wire_connection in WireConnections:
